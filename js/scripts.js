8490b40cc8930f43e748b4cf6e610f1efa863206
Saalim Aqueel <aqueel192@gmail.com>
2022-10-06 13:44:12 +0530

    Changed dark mode issues and UI

--- js/scripts.js
+++ js/scripts.js
@@ -51,4 +51,48 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+    
+ 
+
 });
+
+//Dark mode
+
+var checkbox = document.getElementById("ChangeTheme"); //get the checkbox to a variable
+
+//check storage if dark mode was on or off
+if (sessionStorage.getItem("mode") == "dark") {
+  darkmode(); //if dark mode was on, run this funtion
+} else {
+  nodark(); //else run this funtion
+}
+
+//if the checkbox state is changed, run a funtion
+checkbox.addEventListener("change", function() {
+  //check if the checkbox is checked or not
+  if (checkbox.checked) {
+    darkmode(); //if the checkbox is checked, run this funtion
+  } else {
+    nodark(); //else run this funtion
+  }
+});
+
+//function for checkbox when checkbox is checked
+function darkmode() {
+    const body = document.querySelector('body');
+    const cards = document.querySelectorAll('.card');
+    body.classList.add('dark-mode');
+    cards.forEach(card => card.classList.add('dark-box'));
+  checkbox.checked = true; //set checkbox to be checked state
+  sessionStorage.setItem("mode", "dark"); //store a name & value to know that dark mode is on
+}
+
+//function for checkbox when checkbox is not checked
+function nodark() {
+    const body = document.querySelector('body');
+    const cards = document.querySelectorAll('.card')
+  document.body.classList.remove("dark-mode"); //remove added class from body tag
+  cards.forEach(card => card.classList.remove('dark-box'))
+  checkbox.checked = false; //set checkbox to be unchecked state
+  sessionStorage.setItem("mode", "light"); //store a name & value to know that dark mode is off or light mode is on
+}
