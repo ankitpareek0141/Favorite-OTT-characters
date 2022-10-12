b9c076e47c8d75181e4b714d5d4e5f9fc7cdb54e
Yash Gajjar <yash.gajjar@yudiz.com>
2022-10-12 12:40:24 +0530

    Add: Search field searches on enter click
    closes #105

--- js/index.js
+++ js/index.js
@@ -3,3 +3,9 @@ function doSearch(text) {
         console.log(window.find(text));
     }
 }
+
+document.getElementById('SearchTxt').addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        doSearch(event.target.value);
+    }
+});
