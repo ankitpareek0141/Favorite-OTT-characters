9287d06885a4257d010b550f05f55e81300abfe7
Yash Gajjar <52910101+Yash-Gajjar@users.noreply.github.com>
2022-10-21 10:20:28 +0530

    Fix: Iron Man image URL

--- js/characters.js
+++ js/characters.js
@@ -20,7 +20,7 @@ const ALL_CHARACTERS = [
         "description": "Jon Snow is a fictional character in the A\n                                Song of Ice and Fire series of fantasy\n                                novels by American author George R. R.\n                                Martin, and its television adaptation Game\n                                of Thrones, in which he was portrayed by\n                                English actor Kit Harington. In the novels,\n                                he is a prominent point of view character."
     },
     {
-        "image": "https://static.wikia.nocookie.net/marvelcinematicuniverse/images/3/35/IronMan-EndgameProfile.jpg/revision/latest/scale-to-width-down/350?cb=20190423175213",
+        "image": "http://m.gettywallpapers.com/wp-content/uploads/2020/04/Iron-Man-HD-Background-1024x576.jpg",
         "title": "Iron Man",
         "description": "Iron Man is a superhero appearing in American comic books published by Marvel Comics. The character was co-created by writer and editor Stan Lee, developed by scripter Larry Lieber, and designed by artists Don Heck and Jack Kirby. The character made his first appearance in Tales of Suspense #39 (cover dated March 1963), and received his own title in Iron Man #1 (May 1968). Also in 1963, the character founded the Avengers alongside Thor, Ant-Man, Wasp and the Hulk."
     },
